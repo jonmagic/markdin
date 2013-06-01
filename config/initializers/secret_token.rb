c10cc0669e8d2bb02a076ce98439da5a27136c59
Jonathan Hoyt <hoyt@github.com>
2013-06-01 10:39:59 -0700

    Initialize app with rails new.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Markdin::Application.config.secret_key_base = '8fb92d1348cd489e18c2cf04ad4a905280a228d96301d740b2dfedbd26b94a83fb575e85a93c44662b5d53405434ee4fe6e15a5de175817b719fc646bc47d1a4'
